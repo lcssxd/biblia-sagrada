f6a165c492a6281c4e083dcd2c8d405e2e04623d
Luan Soares <luansesa@hotmail.com>
2024-01-17 22:39:50 -0300

    fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/tailwind.css'
+    '@/assets/css/scrollbar.css',
+    '@/assets/css/tailwind.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
