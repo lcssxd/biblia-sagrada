7e7f5746a9e6a403011449206ee91b0c9ccf761e
Luan Soares <luansesa@hotmail.com>
2024-01-22 02:09:02 -0300

    Fixes header about

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col h-full justify-between overflow-hidden">
-    <Header class="relative flex items-center justify-center">
-      <NuxtLink to="/settings" class="flex items-center absolute left-1 inset-y-0 p-2 cursor-pointer outline-none">
+  <div class="flex flex-col h-full overflow-hidden">
+    <Header class="flex items-center space-x-2">
+      <NuxtLink to="/settings" class="flex items-center p-2 cursor-pointer outline-none">
         <arrowlongleftIcon class="w-5 h-5" />
       </NuxtLink>
       <span>{{ title }}</span>
